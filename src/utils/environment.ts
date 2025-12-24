267250d9b5380b8b7d44294e6bf0b6556ce45ef4
ibrar-web <ibrarjut@yahoo.com>
2025-12-24 15:53:29 +0500

    database conneciton  updated

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -49,6 +49,7 @@ export const environment = {
     },
   },
   database: {
+    url: process.env.DATABASE_URL,
     host: process.env.DATABASE_HOST,
     port: Number(process.env.DATABASE_PORT || "5432"),
     user: process.env.DATABASE_USER,
